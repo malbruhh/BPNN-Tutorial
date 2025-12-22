e25fbafb025929f9bbc82382f27507859b4b0640
malbruhh <ikhmaldanish741@gmail.com>
2025-12-22 19:54:40 +0800

    added nn class and sigmoid func

--- tutorial.py
+++ tutorial.py
@@ -1,6 +1,30 @@
 import numpy as np
 import csv
 
+#how machine learning work in pyhton:
+#   4                                   4                            1 (0 or 1)
+#[INPUT] --*-- [w1] -- [activation + HIDDEN] --*--[w2]--[activation + OUTPUT]
+# 1100x4        4x4                  1100x4       4x1                 1100x1
+#
+# follow linear algebra matrices formula: 
+# Supposed matrices size : AxB * YxZ, the size will be A*Z
+# size of weight will be determined by using the size from layer forward.
+
+#this activation function will use for converting to 0-1 value
+def sigmoid(x):
+    return 1 / (1+ np.exp(-x))
+class NeuralNetwork:
+    def __init__(self, inp, exp):
+        self.input = inp
+        self.exp = exp
+        self.weight1 = np.random.rand(4,4) #in matrices form
+        self.weight2 = np.random.rand(4,1) #in matrices form
+        
+    def feedforward(self):
+        self.hidden = sigmoid(np.dot(self.input, self.weight1)) #multiply with w1 matrices
+        self.output = sigmoid(np.dot(self.hidden, self.weight2)) #multiple with w2 matrices
+        print(self.output)
+
 #data will be seperated into 2 set:
 #[Train] and [Test]
 def main():
@@ -19,20 +43,30 @@ def main():
     #seperate between input and expected_output data
     for line in lines:
         line = line.split(',')
-        input.append([float(x) for x in line[0:3]]) #insert into input data
-        expected.append(float(line[3]))
+        input.append([float(x) for x in line[0:4]]) #insert into input data
+        expected.append(float(line[4]))
 
     #convert into numpy array to use other numpy func
     input = np.array(input)
     expected = np.array(expected)
     
     #split data into training(80%) and test(20%)
-    split_i = int(len(input)*0.8)
-    input_train = input[:split_i]
-    input_test = input[split_i:]
+    # split_i = int(len(input)*0.8)
     
-    expected_train = expected[:split_i]
-    expected_test = expected[split_i:]
-    print(input)
+    # input_train = input[:split_i]
+    # expected_train = expected[:split_i]
+    
+    # input_test = input[split_i:]
+    # expected_test = expected[split_i:]
+    
+    input_train = input[:1100]
+    expected_train = expected[:1100]
+    
+    input_test = input[1100:]
+    expected_test = expected[1100:]
+    
+    nn = NeuralNetwork(input_train,expected_train)
+    nn.feedforward()
+
 if __name__ == '__main__':
     main()
